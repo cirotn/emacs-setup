03af6e5b2b3cf6119a9e604a450fba69c48f828f
Ciro Nishiguchi <cironishiguchi@gmail.com>
2019-12-22 23:12:51 -0600

    Replace ido with ivy

--- my-packages.el
+++ my-packages.el
@@ -1,6 +1,22 @@
 (require 'cl)
 
-(use-package iedit
+(use-package ivy
+  :ensure t
+  :config
+  (ivy-mode t)
+  ;; Set the completion to be fuzzy for files and buffers
+  (setq ivy-re-builders-alist
+        '((read-file-name-internal . ivy--regex-fuzzy)
+          (internal-complete-buffer . ivy--regex-fuzzy)
+          (t . ivy--regex-plus)))
+  ;; Adds recent files to buffer list
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "(%d/%d) "))
+
+(use-package swiper
+  :ensure t)
+
+(use-package counsel
   :ensure t)
 
 (use-package undo-tree
@@ -13,21 +29,22 @@
   :ensure t
   :config
   (evil-mode 1)
-  ;; Make evil forward work with symbols rather than words, in whatever way
-  ;; symbols are defined in the current emacs mode
+  ;; Make underscores part of a word
   (add-hook 'python-mode-hook '(lambda () (modify-syntax-entry ?_ "w")))
   (add-hook 'c++-mode-hook '(lambda () (modify-syntax-entry ?_ "w")))
   (add-hook 'c-mode-hook '(lambda () (modify-syntax-entry ?_ "w")))
   (add-hook 'makefile-mode-hook '(lambda () (modify-syntax-entry ?_ "w")))
   (add-hook 'package-mode-hook '(lambda () (modify-syntax-entry ?_ "w")))
-  ;; Leave M-. for tag, to allow use of C-u M-. for next tag (seems to work better than C-])
-  (define-key evil-normal-state-map (kbd "M-.") nil)
-  ;; Use space to scroll up and down
+  ;; Make hyphen part of a word in lisp
+  (add-hook 'emacs-lisp-mode-hook '(lambda () (modify-syntax-entry ?- "w")))
+  ;; Make asterisk not include \\<...\\>
+  (define-key evil-normal-state-map (kbd "*") 'evil-search-unbounded-word-forward)
+  ;; use space to scroll up and down
   (define-key evil-normal-state-map (kbd "SPC") 'evil-scroll-page-down)
   (define-key evil-normal-state-map (kbd "S-SPC") 'evil-scroll-page-up)
-  ;; Evil doesn't auto-indent in insert mode by default
+  ;; evil doesn't auto-indent in insert mode by default
   (define-key evil-insert-state-map (kbd "RET") 'evil-ret-and-indent)
-  ;; Set shift width according to language
+  ;; set shift width according to language
   (add-hook 'python-mode-hook (lambda () (setq evil-shift-width python-indent)))
   (add-hook 'c++-mode-hook (lambda () (setq evil-shift-width c-basic-offset)))
   (add-hook 'c-mode-hook (lambda () (setq evil-shift-width c-basic-offset)))
@@ -35,20 +52,8 @@
   (add-hook 'package-mode-hook (lambda() (setq evil-shift-width 3)))
   (add-hook 'verilog-mode-hook (lambda() (setq evil-shift-width 2))))
 
-(use-package ido
-  :ensure t
-  :config
-  (ido-mode t)
-  ;; Disable automatic file name completion for recent files, this feature
-  ;; causes the directory to change awkwardly while typing a file name
-  (setq ido-auto-merge-work-directories-length -1)
-  (setq ido-enable-flex-matching t))
-
-(use-package ido-vertical-mode
-  :ensure t
-  :config
-  (ido-vertical-mode 1)
-  (setq ido-vertical-define-keys 'C-n-and-C-p-only))
+(use-package iedit
+  :ensure t)
 
 (use-package p4
   :ensure t)
@@ -79,6 +84,7 @@
   :config
   (global-set-key (kbd "C-x f") 'find-file-in-repository))
 
+;; When switching windows, number them and ask for the destination
 (use-package ace-window
   :ensure t
   :config
@@ -114,6 +120,22 @@
 ;;     (define-key yas-minor-mode-map (kbd "<tab>") nil)
 ;;     (define-key yas-minor-mode-map (kbd "TAB") nil)
 ;;     (define-key yas-minor-mode-map (kbd "<backtab>") 'yas-expand))
+
+;; replaced by ivy
+;;(use-package ido
+;;  :ensure t
+;;  :config
+;;  (ido-mode t)
+;;  ;; Disable automatic file name completion for recent files, this feature
+;;  ;; causes the directory to change awkwardly while typing a file name
+;;  (setq ido-auto-merge-work-directories-length -1)
+;;  (setq ido-enable-flex-matching t))
+;;
+;;(use-package ido-vertical-mode
+;;  :ensure t
+;;  :config
+;;  (ido-vertical-mode 1)
+;;  (setq ido-vertical-define-keys 'C-n-and-C-p-only))
 
 (require 'framemove)
 (setq framemove-hook-into-windmove t)
