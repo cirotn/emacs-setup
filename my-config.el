8c3c65e8ab4b7524c44b26b16d75939ee9ff38cf
Ciro Nishiguchi <ciro@atom-computing.com>
2021-02-20 22:28:51 -0600

    Update path to orgmode

--- my-config.el
+++ my-config.el
@@ -6,14 +6,7 @@
   ;;
   ;; Perforce variables
   (progn
-    (defvar perforce-port "perforce.natinst.com:1666")
-    (defvar penguin-port "penguin.natinst.com:1666")
-    (defvar P4CLIENT "cnishigu_ctn_dt")
-    (setq perforce-clientspec  "cnishigu_ctn-dt")
-    (setq penguin-clientspec   "cnishigu_ctn-dt")
-    (global-set-key (kbd "C-c o o") (lambda() (interactive) (find-file "C:/Users/cnishigu/Dropbox/orgmode")))
-    ;; Exclude all files with boost in the file path from etags to keep file size manageable
-    ;;(setq ni-ctags-ectags-options (concat ni-ctags-ectags-options " --exclude=boost")
+    (global-set-key (kbd "C-c o o") (lambda() (interactive) (find-file "C:/Users/cnishigu/orgmode")))
     ;; Python path
     (setq python-shell-interpreter "C:/Users/cnishigu/AppData/Local/Programs/Python/Python36/pythonw.exe")
     ;; Removes shell warning about readline when starting python
@@ -25,10 +18,10 @@
     (setq python-shell-interpreter "/anaconda3/bin/python3")
     ;; Removes shell warning about readline when starting python
     (setq python-shell-completion-native-enable nil)
-    (global-set-key (kbd "C-c o o") (lambda() (interactive) (find-file "//Users/ciro/Dropbox/orgmode")))))
+    (global-set-key (kbd "C-c o o") (lambda() (interactive) (find-file "//Users/ciro/orgmode")))))
  (t
   ;; Setup for other systems (Linux)
   (progn
-    (global-set-key (kbd "C-c o o") (lambda() (interactive) (find-file "~/Dropbox/orgmode"))))))
+    (global-set-key (kbd "C-c o o") (lambda() (interactive) (find-file "~/orgmode"))))))
 
 (provide 'my-config)
